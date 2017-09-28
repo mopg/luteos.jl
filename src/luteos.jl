1ba58abd07107692bbbb9e1a1be4a5f9f9da864c
Max Opgenoord <mopg@mit.edu>
2017-09-28 19:51:00 -0400

    Added pretty printout at start of solve

--- src/luteos.jl
+++ src/luteos.jl
@@ -17,19 +17,9 @@ module luteos
 
 export hdgSolveElas, Material, Problem, Master2D, Master3D, Mesh2D, Mesh3D, compJacob!, writeTecplot
 
-# include("integration/basisFuncLineLag.jl")
-# include("integration/basisFuncLineLeg.jl")
-# include("integration/basisFuncTriangleLag.jl")
-# include("integration/basisFuncTriangleLeg.jl")
-# include("integration/basisFuncTetLag.jl")
-# include("integration/basisFuncTetLeg.jl")
-#
-# include("integration/quadratureLine.jl")
-# include("integration/quadratureTriangle.jl")
-# include("integration/quadratureTet.jl")
-
 include("general/material.jl")
 include("general/problem.jl")
+include("general/setup.jl")
 
 include("mesh/mesh.jl")
 include("mesh/master.jl")

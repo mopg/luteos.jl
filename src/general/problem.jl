1b006ba5aaad3dcef60668277f900c97a164466f
Max Opgenoord <mopg@mit.edu>
2017-10-02 21:24:56 -0400

    Improved setup scripts

--- src/general/problem.jl
+++ src/general/problem.jl
@@ -29,3 +29,24 @@ type Problem
   bcfunc::Array{Function} # Boundary function for each boundary face
 
 end
+
+"""
+    Problem(src::Function, bcf::Array{Function}, bctype::Array{Int64};
+            name = "Answer to Life Universe and Everything", bcnorm = false)
+
+Constructor for `Problem` type. `src` is a function that returns the source
+function for the problem. `bcf` is an Array of Functions, which specifies the
+boundary condition on each boundary surface.
+"""
+function Problem(src, bcf, bctype;
+                 name = "Answer to Life Universe and Everything", bcnorm = false)
+
+  setup()
+
+  if length(bctype) != length(bcf)
+    error("Problem:: bctype needs have the same size as bcf")
+  end
+
+  Problem(name, src, bctype, bcnorm, bcf)
+
+end
